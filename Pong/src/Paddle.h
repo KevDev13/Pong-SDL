7a943f1eefa4d4bc636ecac0d9f983cf53837ed7
Kevin Garner <kg.dev@protonmail.com>
2018-10-07 14:55:12 -0500

    Paddle displays on screen

--- Pong/src/Paddle.h
+++ Pong/src/Paddle.h
@@ -3,18 +3,33 @@ Paddle class
 */
 
 #include <stdint.h>
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
 
 #pragma once
 class Paddle
 {
 public:
 	Paddle();
+	Paddle(uint8_t player, bool isComputerControlled);
 	~Paddle();
-	inline void setComputerControlled() { m_bIsComputerControlled = true; }
-	void setPlayerControlled(uint8_t p);
+	void setPlayerControlled(bool b, uint8_t p = 0);
+	bool LoadDefaultImage(SDL_Renderer* &renderer);
+	bool LoadImage(SDL_Renderer* &renderer, std::string file);
+
+	inline uint8_t getPlayer() const { return _player; }
+	inline bool isComputerControlled() const { return _bIsComputerControlled; }
+	inline bool isPlayerControlled() const { return !_bIsComputerControlled; }
+	inline SDL_Texture* GetImage() const { return _image; }
+	inline SDL_Rect GetRectangle() const { return rectangle; }
 
 private:
-	uint8_t m_player;	// player 1 or 2
-	bool m_bIsComputerControlled;	// is the computer controlling this paddle
+	uint8_t _player;	// player 1 or 2
+	bool _bIsComputerControlled;	// is the computer controlling this paddle?
+	SDL_Texture* _image;
+	SDL_Rect rectangle;
+
+	static const std::string sImageFileName;
 };
 
