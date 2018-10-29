27676801c41ecde3162d583ce04904b9496769ca
Kevin Garner <kg.dev@protonmail.com>
2018-10-28 20:43:09 -0500

    started work on Texture class

--- Pong/src/Texture.cpp
+++ Pong/src/Texture.cpp
@@ -1,11 +1,28 @@
 #include "Texture.h"
 
-
-
 Texture::Texture()
 {
 }
 
+bool Texture::LoadImage(SDL_Renderer* &renderer, std::string file)
+{
+	SDL_Surface* loadedImage = nullptr;
+	SDL_Texture* texture = nullptr;
+
+	// load image from file
+	loadedImage = IMG_Load(file.c_str());
+
+	if (loadedImage == nullptr)
+	{
+		_image = nullptr;
+		return false;
+	}
+
+	texture = SDL_CreateTextureFromSurface(renderer, loadedImage);
+	SDL_FreeSurface(loadedImage);
+	_image = texture;
+	return true;
+}
 
 Texture::~Texture()
 {
--- Pong/src/Texture.h
+++ Pong/src/Texture.h
@@ -1,8 +1,22 @@
 #pragma once
+
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
+
 class Texture
 {
 public:
 	Texture();
 	~Texture();
+
+	bool LoadImage(SDL_Renderer* &renderer, std::string file);
+
+	inline SDL_Texture* GetImage() const { return _image; }
+	inline SDL_Rect* GetRectangle() const { return _rectangle; }
+
+private:
+	SDL_Texture* _image;
+	SDL_Rect* _rectangle;
 };
 
